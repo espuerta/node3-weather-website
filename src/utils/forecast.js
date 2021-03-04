abe037c195539f690edf78b6bcd63ce7e24d5614
Janellis Espuerta <espuertajel@gmail.com>
2021-03-04 09:45:27 +0800

    Add high and low t0 f0rcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,7 @@
 const request = require('request')
+
+
+
 const forecast = (latitude, longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=868ece29e070fe5d77b32727187e308e&query=' + latitude  + ',' + longitude + '&units=f'
@@ -11,7 +14,8 @@ const forecast = (latitude, longitude, callback) => {
              }else if(body.error){
                 callback('Unable to find location', undefined)
              }else{
-                 callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degrees out ' + ' but feels likes ' + body.current.feelslike + ' degrees out ')
+                
+                 callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degrees out ' + ' It feels likes ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%')
                  
              }
         })
