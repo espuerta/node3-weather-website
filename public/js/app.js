99db030538580edd70f39dcc65cf1b1dec57b279
Janellis Espuerta <espuertajel@gmail.com>
2021-03-04 08:41:24 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
             const location = search.value
         messageOne.textContent = 'Loading...'
         messageTwo.textContent =   ''
-            fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+            fetch('/weather?address=' + location).then((response) => {
                 response.json().then((data) => {
                     if(data.error){
                         messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const  geocode = require('./utils/gecode')
 const forecast = require('./utils/forecast')
 
 const app  = express()
-
+const port = process.env.PORT || 3000
 //Define paths or express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -106,6 +106,6 @@ app.get('*', (req, res) => {
 })
         
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
